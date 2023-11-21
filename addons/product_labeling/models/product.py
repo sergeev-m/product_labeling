58b4d7197c0060b42fbf4f6f2fe79d7ae201866d
Mikhail Sergeev <server-15@yandex.ru>
2023-11-21 07:49:43 +0500

    many products in the act

--- addons/product_labeling/models/product.py
+++ addons/product_labeling/models/product.py
@@ -1,6 +1,7 @@
 import logging
 
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 from  odoo.service.server import _logger
 
 # _logger = logging.getLogger(__name__)
@@ -64,6 +65,7 @@ class MarkedProduct(models.Model):
         column2='marked_product_id',
         string='Акты'
     )
+
     expenses_receipts_ids = fields.One2many(
         comodel_name='product.marked_product_expenses_receipts',
         inverse_name='act_id',
@@ -73,6 +75,13 @@ class MarkedProduct(models.Model):
     )
     total_amount = fields.Monetary(string='Прибыль', compute='_compute_total_amount', readonly=True)
     currency_id = fields.Many2one('res.currency', default=lambda self: self.env.ref('base.RUB'), readonly=True) # todo
+    amount = fields.Integer('Количество товара')
+
+    @api.constrains('amount')
+    def _check_amount_positive(self):
+        for record in self:
+            if record.amount < 0:
+                raise ValidationError('Количество товара должно быть больше нуля.')  # todo
 
     def _compute_expenses_receipts(self):
         for marked_product in self:
@@ -81,7 +90,6 @@ class MarkedProduct(models.Model):
                 [('act_id', 'in', marked_product.act_ids.ids)]
             )
 
-    # @api.depends('expenses_receipts_ids')
     def _compute_total_amount(self):
         for marked_product in self:
             p = marked_product.expenses_receipts_ids.mapped('value')
@@ -102,6 +110,56 @@ class MarkedProduct(models.Model):
         for record in self:
             record.name = f'{record.product_id.name} #{record.id}'
 
+    def copy(self, default=None):
+        if default is None:
+            default = {}
+        new_record = super().copy(default)
+
+        act_mapping = {}
+
+        # for act in self.act_ids:
+        #     new_act = act.copy({'marked_product_ids': [(4, new_record.id)]})
+        #     act_mapping[act.id] = new_act.id
+
+        # new_act_ids = [(6, 0, list(act_mapping.values()))]
+        # new_record.write({'act_ids': new_act_ids})
+
+
+        # for related_record in self.act_ids:
+        #     related_record.copy({'marked_product_id': new_record.id})
+
+        act_ids = [(4, act.id) for act in self.act_ids]
+
+        # act_mapping = {}
+        #
+        # for act in self.act_ids:
+        #     new_act = act.copy({'marked_product_ids': new_record.id})
+        #     act_mapping[act.id] = new_act.id
+        #
+        # new_act_ids = [(6, 0, list(act_mapping.values()))]
+        new_record.write({'act_ids': act_ids})
+
+
+
+        # for act in self.act_ids:
+        #     new_act = act.copy({'marked_product_id': new_record.id})
+        #     id_mapping[act.id] = new_act.id
+
+        # new_act_ids = [(6, 0, list(act_mapping.values()))]
+        # new_record.write({'act_ids': new_act_ids})
+
+        # for act in self.act_ids:
+        #     new_act = act.copy()
+        #     id_mapping[act.id] = new_act.id
+
+        # new_act_ids = [(4, i.id) for i in self.act_ids]
+        # new_record.write({'act_ids': new_act_ids})
+        #
+        # # act_ids = [act.id for act in self.act_ids]
+        # # new_record.write({'act_ids': new_act_ids})
+        #
+        return new_record
+
 
 class ProductAct(models.Model):
     _name = 'product.act'
@@ -109,10 +167,12 @@ class ProductAct(models.Model):
 
     name = fields.Char('Название', default=lambda self: self._get_default_value())
     product_id = fields.Many2one('product.product', string='Продукт')
+
     # marked_product_id = fields.Many2one(
     #     'product.marked_product',
     #     string='Маркированный продукт'
     # )
+
     marked_product_ids = fields.Many2many(
         'product.marked_product',
         relation='marked_product_act_rel',
@@ -120,9 +180,11 @@ class ProductAct(models.Model):
         column2='akt_id',
         string='Маркированный продукт'
     )
+
     warehouse_from_id = fields.Many2one(
         'product.warehouse',
-        string='Применить для товаров со склада'
+        string='Применить для товаров со склада',
+        default=lambda self: self.marked_product_ids.warehouse_id.id or None
     )
     warehouse_to_id = fields.Many2one(
         'product.warehouse',
@@ -166,16 +228,38 @@ class ProductAct(models.Model):
         for rec in self:
             if rec.status_id.name.lower() == 'покупка':
                 values = {
-                    'product_id': self.product_id.id,
-                    'warehouse_id': self.warehouse_to_id.id,
-                    'status_id': self.status_id.id,
+                    'product_id': rec.product_id.id,
+                    'warehouse_id': rec.warehouse_to_id.id,
+                    'status_id': rec.status_id.id,
+                    'amount': rec.amount,
                     'act_ids': [rec.id]
                 }
-                marked_product_env.create([values for _ in range(rec.amount)])
+                marked_product_id = marked_product_env.create(values)
+                rec.write({'marked_product_ids': marked_product_id})
+
             else:
-                rec.marked_product_ids.write({
-                    'warehouse_id': rec.warehouse_to_id.id,
-                    'status_id': rec.status_id.id
-                })
+                for product in rec.marked_product_ids:
+                    old_amount = product.amount
+                    values = {
+                        'warehouse_id': rec.warehouse_to_id.id,
+                        'status_id': rec.status_id.id,
+                        'amount': rec.amount,
+                        'act_ids': [(4, rec.id, 0)]
+                    }
+
+                    if not old_amount - rec.amount:
+                        if rec.status_id.name.lower() == 'продажа':
+                            values['amount'] = 0
+                        product.write(values)
+
+                    else:
+                        if rec.status_id.name.lower() == 'продажа':
+                            values['amount'] = 0
+                        product.write({'amount': old_amount - rec.amount})
+
+
+                        new_marked = product.copy(values)
+                        # rec.write({'marked_product_ids': [(6, 0, [new_marked.id])]})
+                        rec.write({'marked_product_ids': [(3, product.id)]})
 
             rec.write({'is_carried_out': True})
